787c7d807f9d052d7a98462a87586d726cab6d69
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2022-02-01 23:41:07 +0100

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Github App</h1>
+    <GitHubTokenForm />
+    <User />
+    <Repos />
+  </div>
 </template>
 
 <script>
+import GitHubTokenForm from "./components/GitHubTokenForm.vue";
+import Repos from "./components/Repos.vue";
+import User from "./components/User.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    GitHubTokenForm,
+    Repos,
+    User,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
